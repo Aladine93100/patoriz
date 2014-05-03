63aabd0e300b91e573ec7926c033ddb793d14593
Aladine <alazaiane@yahoo.fr>
2014-05-03 17:29:36 +0200

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Resto::Application.config.secret_key_base = '9ac3fc3ae4f826c9c79952807fa26346099ef574bf25f27a2ae828c61acf2c3b119838d64812626d52a619fc5e542a7b0a7cbd604e71849365543d1092b0122e'
