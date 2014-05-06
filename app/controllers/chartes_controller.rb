4541ccdfb09c491f603989b1873afa007b0a4914
Aladine <alazaiane@yahoo.fr>
2014-05-07 00:40:43 +0200

    prix menu

---
+++ app/controllers/chartes_controller.rb
@@ -0,0 +1,4 @@
+class ChartesController < ApplicationController
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Resto::Application.routes.draw do
   get "home/index"
+  get "chartes/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
